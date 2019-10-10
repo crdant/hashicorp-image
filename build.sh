c307d672746a84a910da38797e2bfe281f0c6bdd
Chuck D'Antonio <cdantonio@pivotal.io>
2019-10-09 21:09:11 -0400

    Build the tarball with one script

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 function download() {
   product=$1
   version=$2
-  
+
   pushd work 2>&1 1> /dev/null
     curl -Os https://releases.hashicorp.com/${product}/${version}/${product}_${version}_linux_amd64.zip
     curl -Os https://releases.hashicorp.com/${product}/${version}/${product}_${version}_SHA256SUMS
@@ -15,10 +15,36 @@ function download() {
 
     # trust the binary if the checksum matches
     if shasum -a 256 -c ${product}_${version}_SHA256SUMS 2>/dev/null | grep "OK" ; then
-      unzip ${product}_${version}_linux_amd64.zip && mv ${product} ../bin
+      unzip ${product}_${version}_linux_amd64.zip && mv ${product} ../build/rootfs/bin
     fi
   popd 2>&1 1>/dev/null
 }
 
+function build() {
+  pushd build 2>&1 1> /dev/null
+    tar -cvzf hashicorp-image.tgz bin rootfs
+  popd 2>&1 1>/dev/null
+}
+
+function prepare() {
+  pushd work 2>&1 1> /dev/null
+    if [ ! -d work ] ; then
+      mkdir work
+    fi
+
+    if [ ! -d build ] ; then
+      mkdir build
+      mkdir build/rootfs
+      mkdir build/rootfs/bin
+      pushd build 2>&1 1>/dev/null
+        ln -s ./rootfs/bin bin
+      popd 2>&1 1>/dev/null
+
+    fi
+  popd 2>&1 1>/dev/null
+}
+
+prepare
 download terraform 0.12.10
 download vault 0.5.2
+build
